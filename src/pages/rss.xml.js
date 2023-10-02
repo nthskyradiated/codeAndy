f88f226b0713359ab14dd9d884d96774e73feefa
nthskyradiated <andy.pandaan@outlook.com>
2023-10-02 10:43:44 +0400

    working on rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,9 +3,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
   return rss({
     title: 'codeandy | Blog',
-    description: 'A website to debug the mind',
+    description: 'Debug the Mind',
     site: 'http://localhost:4321',
-    items: await pagesGlobToRssItems(import.meta.glob('./blog/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
 
   })
