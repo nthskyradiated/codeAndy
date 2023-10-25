8d48dad7e960e3c5c963b5e27095ba66e8adc788
nthskyradiated <andy.pandaan@outlook.com>
2023-10-25 13:45:56 +0400

    switched to SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nthsky.me'
+  site: 'https://nthsky.me',
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
