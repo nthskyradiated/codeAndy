1ed63b39cbb476c2acf743702e3f1cb2a05f5fc4
nthskyradiated <andy.pandaan@outlook.com>
2023-10-27 00:10:02 +0400

    set up rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'codeandy | Blog',
+    title: 'Debug The Mind | Blog',
     description: 'Debug the Mind',
-    site: 'https://codeandy.dev',
+    site: 'https://nthsky.me',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
 
