8a18b338f04eb733be968be6d24fe1130723167a
nthskyradiated <andy.pandaan@outlook.com>
2023-10-28 16:33:13 +0400

    reenable astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nthsky.me'
+  site: 'https://nthsky.me',
+  output: "server",
+  adapter: vercel()
 });
