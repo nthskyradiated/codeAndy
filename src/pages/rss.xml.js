2b926a372e0d41c0169e638ee9169e00a42fcaa3
nthskyradiated <andy.pandaan@outlook.com>
2024-10-27 21:53:19 +0400

    upgraded to Astro 4.x

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { pagesGlobToRssItems, rss } from '@astrojs/rss';
+
 
 export async function GET(context) {
   return rss({
-    title: 'Debug The Mind',
-    description: 'Debug the Mind',
-    site: 'https://debugthemind.art',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
 
-  })
+    title: 'Debug The Mind',
+
+    description: 'Debug the Mind',
+
+    site: 'https://debugthemind.art',
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+
+    customData: `<language>en-us</language>`,
+  });
 }
