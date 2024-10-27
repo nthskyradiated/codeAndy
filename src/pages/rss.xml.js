734b0f791a2c2568788c6adfaff530403abcd5be
nthskyradiated <andy.pandaan@outlook.com>
2024-10-27 22:21:21 +0400

    fixed rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,16 @@
-import { pagesGlobToRssItems, rss } from '@astrojs/rss';
-
-
+import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 export async function GET(context) {
   return rss({
-
     title: 'Debug The Mind',
 
-    description: 'Debug the Mind',
+    description: 'posts about web development, open-source, and devops.',
 
     site: 'https://debugthemind.art',
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./blog/*.{md,mdx}'),
+    ),
 
     customData: `<language>en-us</language>`,
   });
